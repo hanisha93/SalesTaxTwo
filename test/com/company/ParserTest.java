7b7a81471f2ca1a8d9a7e9a4dd96205e54a3ee03
Hanisha <hanishap@thoughtworks>
2015-06-15 10:51:09 +0530

    Added spec for parser is calling salestax for non exceptional items and imported items

--- test/com/company/ParserTest.java
+++ test/com/company/ParserTest.java
@@ -8,23 +8,33 @@ import static org.mockito.Mockito.mock;
 public class ParserTest {
 
     @Test
-    public  void specToCheckParserReturnsSalesTaxForExceptionalItems() {
+    public void specToCheckParserReturnsSalesTaxForExceptionalItems() {
         SalesTax salesTax = mock(SalesTax.class);
         Parser parser = new Parser();
         salesTax = parser.parser("1 book at 25.9");
 
-        SalesTax expectedSalesTax = new SalesTax(1,"book",25.9 ,0);
-        assertEquals(salesTax,expectedSalesTax);
+        SalesTax expectedSalesTax = new SalesTax(1, "book", 25.9, 0);
+        assertEquals(salesTax, expectedSalesTax);
     }
 
     @Test
-    public  void specToCheckParserReturnsSalesTaxForExceptionalAndImportedItems() {
+    public void specToCheckParserReturnsSalesTaxForExceptionalAndImportedItems() {
         SalesTax salesTax = mock(SalesTax.class);
         Parser parser = new Parser();
         salesTax = parser.parser("1 imported book at 25.9");
 
-        SalesTax expectedSalesTax = new SalesTax(1,"book",25.9 ,5);
-        assertEquals(salesTax,expectedSalesTax);
+        SalesTax expectedSalesTax = new SalesTax(1, "book", 25.9, 5);
+        assertEquals(salesTax, expectedSalesTax);
+    }
+
+    @Test
+    public void specToCheckParserReturnsSalesTaxForNonExceptionalAndImportedItems() {
+        SalesTax salesTax = mock(SalesTax.class);
+        Parser parser = new Parser();
+        salesTax = parser.parser("1 imported music cd at 25.9");
+
+        SalesTax expectedSalesTax = new SalesTax(1, "musiccd", 25.9, 15);
+        assertEquals(salesTax, expectedSalesTax);
     }
 
 }
