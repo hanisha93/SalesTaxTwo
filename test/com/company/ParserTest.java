b5819c793a2d0f3e616fc253fe2bdaf7d9ba0a40
Hanisha <hanishap@thoughtworks>
2015-06-15 10:48:37 +0530

    Added spec for parser is calling salestax for exceptional items and imported items

--- test/com/company/ParserTest.java
+++ test/com/company/ParserTest.java
@@ -8,7 +8,7 @@ import static org.mockito.Mockito.mock;
 public class ParserTest {
 
     @Test
-    public  void specToCheckParserReturnsCorrectSalesTaxObject() {
+    public  void specToCheckParserReturnsSalesTaxForExceptionalItems() {
         SalesTax salesTax = mock(SalesTax.class);
         Parser parser = new Parser();
         salesTax = parser.parser("1 book at 25.9");
@@ -16,4 +16,15 @@ public class ParserTest {
         SalesTax expectedSalesTax = new SalesTax(1,"book",25.9 ,0);
         assertEquals(salesTax,expectedSalesTax);
     }
+
+    @Test
+    public  void specToCheckParserReturnsSalesTaxForExceptionalAndImportedItems() {
+        SalesTax salesTax = mock(SalesTax.class);
+        Parser parser = new Parser();
+        salesTax = parser.parser("1 imported book at 25.9");
+
+        SalesTax expectedSalesTax = new SalesTax(1,"book",25.9 ,5);
+        assertEquals(salesTax,expectedSalesTax);
+    }
+
 }
